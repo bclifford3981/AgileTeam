ed2ca512f6ee411c34bc163af528bf71debf4d64
PeterAstle <79322373+PeterAstle@users.noreply.github.com>
2021-04-16 08:45:22 -0400

    ticket 3 initial commit

--- AgentModels/AgentListItem.cs
+++ AgentModels/AgentListItem.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using AgentData;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgentModels
 {
-    class AgentListItem
+   public class AgentListItem
     {
+        public int AgentId { get; set; }
+
+        public string Name { get; set; }
+
+        public FieldType FieldType { get; set; }
+
+        [Display(Name ="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
